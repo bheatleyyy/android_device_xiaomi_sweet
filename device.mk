46c2a8a2d5f76488756523db7cc071b15eea4512
Yung Ti Su <andysu@google.com>
2020-04-27 14:45:07 +0800

    sweet: Enabling sound trigger V2_3
    Bug: 139071862
    Test: manual
    
    Signed-off-by: Yung Ti Su <andysu@google.com>
    Change-Id: I4123b0af64fc7bed934026540136e3811fab32f3
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- device.mk
+++ device.mk
@@ -31,8 +31,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl
 
 PRODUCT_PACKAGES += \
-    android.hardware.soundtrigger@2.1-impl \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
