aeb9419ed52ee388f875383c1e4a75d7b1c77e0e
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2020-12-03 13:09:27 +0530

    sweet: Include hardware/google/interfaces and hardware/google/pixel  * Required by power-libperfmgr
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- device.mk
+++ device.mk
@@ -185,7 +185,9 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Perf
 PRODUCT_PACKAGES += \
