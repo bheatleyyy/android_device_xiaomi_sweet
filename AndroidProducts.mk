320f99839533704d8443b846898f624c8a9f664c
bheatleyyy <sugakesshaa@gmail.com>
2021-11-07 04:32:10 +0000

    sweet: syberia bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_sweet.mk
+    $(LOCAL_DIR)/syberia_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_sweet-eng \
-    arrow_sweet-userdebug \
-    arrow_sweet-user
+    syberia_sweet-eng \
+    syberia_sweet-userdebug \
+    syberia_sweet-user
