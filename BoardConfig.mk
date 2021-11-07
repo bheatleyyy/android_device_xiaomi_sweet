298dcffe3dff543e54ed6a9ca576ad3cd7ae2d25
bheatleyyy <sugakesshaa@gmail.com>
2021-11-07 04:35:04 +0000

    sweet: go permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom \
                         service_locator.enable=1 \
                         swiotlb=1 \
                         androidboot.usbcontroller=a600000.dwc3
-
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
